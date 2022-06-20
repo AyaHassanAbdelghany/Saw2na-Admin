55917faefbfd0772abec5f7dc82b912da66ee975
Karam Ibrahim <karamzeroo@gmail.com>
2022-06-20 19:01:30 +0200

    testing

--- app/src/test/java/com/example/mcommerceadminapp/coupon/data_source/CouponRepoTest.kt
+++ app/src/test/java/com/example/mcommerceadminapp/coupon/data_source/CouponRepoTest.kt
@@ -7,6 +7,8 @@ import com.example.mcommerceadminapp.model.remote_source.coupon.CouponRemoteSour
 import com.example.mcommerceadminapp.model.shopify_repository.coupon.CouponRepo
 import com.example.mcommerceadminapp.pojo.coupon.price_rule.PriceRules
 import getOrAwaitValue
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers
 import org.hamcrest.MatcherAssert
@@ -14,11 +16,12 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
 import org.robolectric.annotation.Config
 
 @Config(sdk = [30])
 @RunWith(AndroidJUnit4::class)
-class CouponRepoTest{
+class CouponRepoTest :TestCase(){
 
 private lateinit var couponRepo : CouponRepo
 private val pricRule = PriceRules(title = "SUMMERSALE10OFF")
@@ -29,21 +32,20 @@ private val pricRule = PriceRules(title = "SUMMERSALE10OFF")
     @Before
     fun createRepository() {
     // Get a reference to the class under test
-    couponRepo = CouponRepo.getInstance(
-        CouponRemoteSource.getInstance()
+        couponRepo = CouponRepo.getInstance(
+        FakeDataSource.getInstance()
     )
    }
 
 
    @Test
-   fun getAllPriceRules_postValueInAllPriceRules()  =  runBlockingTest {
+   fun getAllPriceRules_postValueInAllPriceRules()  =  runBlocking {
     //Given repo
-    couponRepo.getAllPriceRules()
+       couponRepo.getAllPriceRules()
     // Then
        val value = CouponRepo.allPriceRules.getOrAwaitValue()
        MatcherAssert.assertThat(
-           value, CoreMatchers.not(CoreMatchers.nullValue())
-       )
+           value, CoreMatchers.nullValue())
        //Assert.assertEquals(allPriceRules.data.get(0).title,pricRule.title )
    }
 }
